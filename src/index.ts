f37e6acd61aa26ecd003bf229f79a49e94e400ac
Satoshi Watanabe <sassy.watanabe@gmail.com>
2021-09-19 21:52:44 +0900

    add express and mongodb client.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import { ApolloServer, gql, } from "apollo-server";
+import { ApolloServer, gql, } from "apollo-server-express";
+import * as express from "express";
 import { GraphQLScalarType } from "graphql";
+import expressPlayground  from "graphql-playground-middleware-express";
 import * as dayjs from 'dayjs';
+import { MongoClient } from "mongodb";
 
 const typeDefs = gql`
 scalar DateTime
@@ -129,11 +132,33 @@ const resolvers = {
   })
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
+async function startApolloServer() {
+  const MONGO_DB = "mongodb://localhost:27017/test"
+  const client = await MongoClient.connect(
+    MONGO_DB,
+  );
+  const db = client.db();
+  const context = {db};
 
-server.listen().then(({url}) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context
+  });
+  await server.start();
+
+  const app = express();
+  server.applyMiddleware({app});
+
+  app.get('/', (req:express.Request, res: express.Response) => res.end('Welcome to Photo API'));
+  app.get('/playground', expressPlayground({endpoint: '/graphql'}));
+
+  await new Promise(resolve => app.listen({port: 4010}, () => {
+    console.log(`🚀  Server ready at http://localhost:4010${server.graphqlPath}`);
+  }));
+  
+  return {server, app};
+}
+
+startApolloServer();
+

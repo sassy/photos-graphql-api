4ff8d28796f76352019663cd2d229458f493138f
Satoshi Watanabe <sassy.watanabe@gmail.com>
2021-09-19 17:44:19 +0900

    add tagged Users.

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ type User {
   name: String
   avatar: String
   postedPhotos: [Photo!]!
+  inPhotos: [Photo!]!
 }
 
 enum PhotoCategory {
@@ -25,6 +26,7 @@ type Photo {
   description: String
   category: PhotoCategory!
   postedBy: User!
+  taggedUsers: [User!]!
 }
 
 input PostPhotoInput {
@@ -47,6 +49,13 @@ const users = [
   { 'githubLogin' : 'mHattrup', 'name': 'Mike Hattrup'},
   { 'githubLogin' : 'gPlake', 'name': 'Glen Plake'},
   { 'githubLogin' : 'sSchmldt', 'name': 'Scot Schmidt'}
+]
+
+const tags = [
+  {"photoID": "1", "userID": "gPlake"},
+  {"photoID": "2", "userID": "sSchmldt"},
+  {"photoID": "2", "userID": "mHattrup"},
+  {"photoID": "2", "userID": "gPlake"}
 ]
 
 let _id = 0;
@@ -91,10 +100,18 @@ const resolvers = {
   },
   Photo: {
     url: (parent:any) => `http://yoursite.com/img/${parent.id}.jpg`,
-    postedBy: (parent:any) =>  users.find(u => u.githubLogin === parent.githubUser)
+    postedBy: (parent:any) =>  users.find(u => u.githubLogin === parent.githubUser),
+    taggedUsers: (parents:any) => tags
+      .filter(tag => tag.photoID === parents.id)
+      .map(tag => tag.userID)
+      .map(userID => users.find(u => u.githubLogin === userID))
   },
   User: {
-    postedPhotos: (parent:any) => photos.filter(p => p.githubUser === parent.githubLogin)
+    postedPhotos: (parent:any) => photos.filter(p => p.githubUser === parent.githubLogin),
+    inPhotos: (parent: any) => tags
+      .filter(tag => tag.userID === parent.id)
+      .map(tag => tag.photoID)
+      .map(photoID => photos.find(p => p.id === photoID))
   }
 };
 
